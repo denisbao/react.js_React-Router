818dba52e4ddf4518d498d740e4d6eb2b3ec737c
denisbao <denis.bao@cowmed.com.br>
2022-07-27 08:25:48 -0300

    274 - Using Switch and Exact for configuring routes

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,13 +1,18 @@
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 const ProductDetail = () => {
   const params = useParams()
 
   return (
-    <section>
-      <h1>Product Details</h1>
-      <p>{params.productId}</p>
-    </section>
+    <>
+      <section>
+        <h1>Product Details</h1>
+        <p>{params.productId}</p>
+      </section>
+      <div>
+        <Link to={'/products'}>Go Back</Link>
+      </div>
+    </>
   )
 }
 
