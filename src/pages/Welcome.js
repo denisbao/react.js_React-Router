928f9ff5cd21704a3a272263bc6af955011a429c
denisbao <denis.bao@cowmed.com.br>
2022-07-27 09:04:17 -0300

    275 - Organizing the routes (fix)

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,12 +1,7 @@
-import { Route } from 'react-router-dom'
-
 const Welcome = () => {
   return (
     <section>
       <h1>Welcome Page</h1>
-      <Route path="/welcome/new-user">
-        <p>Welcome, new user!</p>
-      </Route>
     </section>
   )
 }
